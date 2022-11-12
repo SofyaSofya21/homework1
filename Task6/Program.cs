1e77aaec1c18100cc660f8eba0d8566ee411238b
Sofya <sofyaguseva1@gmail.comgit>
2022-11-12 02:03:56 +0100

    Task 6 created

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число и сообщает, является ли оно четным
+
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int even = number%2;
+
+if(even == 0)
+{
+    Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ является четным");
+}
